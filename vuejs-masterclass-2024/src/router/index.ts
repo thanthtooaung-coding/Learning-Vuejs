a4fce6ba08a7279eba77ada8d8262dfee83b4120
thanthtooaung-coding <thanthtoo1285@gmail.com>
2024-12-14 17:27:34 +0630

    Add unplugin vue router

--- vuejs-masterclass-2024/src/router/index.ts
+++ vuejs-masterclass-2024/src/router/index.ts
@@ -1,34 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
-import HomeView from '@/views/HomeView.vue'
-import { h } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue'),
-    },
-    {
-      // /projects/asdf123
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue'),
-    },
-    {
-      // nowhere/.....
-      path: '/:catchAll(.*)*',
-      name: 'NotFound',
-      component: h('p', { style: 'color: red' }, '404 Not Found'),
-    },
-  ],
+  routes,
 })
 
 export default router
