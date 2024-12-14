1cca3f724ddff5a49c91e8ff65158fba376ca4aa
thanthtooaung-coding <thanthtoo1285@gmail.com>
2024-12-14 18:01:33 +0630

    Refactor Codebase for File-Based Routing

--- vuejs-masterclass-2024/eslint.config.js
+++ vuejs-masterclass-2024/eslint.config.js
@@ -12,6 +12,14 @@ export default [
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
+  {
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: [],
+      },
+    ],
+  },
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
--- vuejs-masterclass-2024/src/pages/index.vue
+++ vuejs-masterclass-2024/src/pages/index.vue
@@ -4,5 +4,7 @@
   <div>
     <h1>Home Page</h1>
     <RouterLink to="/projects">Go to projects</RouterLink>
+    |
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to projects 1</RouterLink>
   </div>
 </template>
