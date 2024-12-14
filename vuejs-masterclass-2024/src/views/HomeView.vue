867277d8ae8b02c4032327430e9d2910fbc271e8
thanthtooaung-coding <thanthtoo1285@gmail.com>
2024-12-14 16:24:20 +0630

    Add vue router

---
+++ vuejs-masterclass-2024/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+export default {
+  name: 'HomeView',
+}
+</script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <RouterLink to="/projects">Go to projects</RouterLink>
+  </div>
+</template>
---
+++ vuejs-masterclass-2024/src/views/ProjectsView.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+export default {
+  name: 'ProjectsView',
+}
+</script>
+
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <RouterLink to="/">Go to home</RouterLink>
+  </div>
+</template>
