4cf5cde4657e79d8f650d9bd3dea758a33ced083
thanthtooaung-coding <thanthtoo1285@gmail.com>
2024-12-14 16:27:10 +0630

    Add lazy routes with dynamic imports

--- vuejs-masterclass-2024/src/router/index.ts
+++ vuejs-masterclass-2024/src/router/index.ts
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
-import ProjectsView from '@/views/ProjectsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +13,7 @@ const router = createRouter({
     {
       path: '/projects',
       name: 'projects',
-      component: ProjectsView,
+      component: () => import('@/views/ProjectsView.vue'),
     },
   ],
 })
